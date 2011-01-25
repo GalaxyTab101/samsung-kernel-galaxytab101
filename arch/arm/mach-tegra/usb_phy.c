45d4a1392b7b1ce217fe6b8906008e82aebfdad0
Scott Williams <scwilliams@nvidia.com>
2011-01-25 13:30:15 -0800

    arm: tegra: Fix compiler warnings
    Original-Change-Id: I0914792c5fa170473d81eaad3c78582d23909f69
    Reviewed-on: http://git-master/r/16950
    Reviewed-by: Jin Qian <jqian@nvidia.com>
    Tested-by: Jin Qian <jqian@nvidia.com>
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    Change-Id: I370dfc82d528a06e820e0e321f48c8c0b67ecac5

--- arch/arm/mach-tegra/usb_phy.c
+++ arch/arm/mach-tegra/usb_phy.c
@@ -869,9 +869,9 @@ static int ulpi_phy_power_on(struct tegra_usb_phy *phy)
 	int ret;
 	unsigned long val;
 	void __iomem *base = phy->regs;
+#ifdef CONFIG_ARCH_TEGRA_2x_SOC
 	struct tegra_ulpi_config *config = phy->config;
 
-#if CONFIG_ARCH_TEGRA_2x_SOC
 	gpio_direction_output(config->reset_gpio, 0);
 	msleep(5);
 	gpio_direction_output(config->reset_gpio, 1);
@@ -952,12 +952,12 @@ static void ulpi_phy_power_off(struct tegra_usb_phy *phy)
 {
 	unsigned long val;
 	void __iomem *base = phy->regs;
+#ifdef CONFIG_ARCH_TEGRA_2x_SOC
 	struct tegra_ulpi_config *config = phy->config;
 
 	/* Clear WKCN/WKDS/WKOC wake-on events that can cause the USB
 	 * Controller to immediately bring the ULPI PHY out of low power
 	 */
-#ifdef CONFIG_ARCH_TEGRA_2x_SOC
 	val = readl(base + USB_PORTSC1);
 	val &= ~(USB_PORTSC1_WKOC | USB_PORTSC1_WKDS | USB_PORTSC1_WKCN);
 	writel(val, base + USB_PORTSC1);
