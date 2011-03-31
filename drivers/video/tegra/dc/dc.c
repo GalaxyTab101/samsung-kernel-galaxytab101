2e12d89ac51fe1358f0b01b9ff5cb4978fdcbe9d
Jon Mayo <jmayo@nvidia.com>
2011-03-30 20:23:39 -0700

    ARM: tegra: dc: fix deadlock on overlay wq flush
    dc lock is head while flush occurred, but functions called by workers need
    take the dc lock to proceed.
    
    Bug 807015
    
    Original-Change-Id: Id30c5c768947f8f9959b01be3983761a18c5a1e5
    Reviewed-on: http://git-master/r/24890
    Reviewed-by: Jonathan Mayo <jmayo@nvidia.com>
    Tested-by: Jonathan Mayo <jmayo@nvidia.com>
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>
    Change-Id: I6537018e03cfc354af9a7187d2fdd019cfe55960

--- drivers/video/tegra/dc/dc.c
+++ drivers/video/tegra/dc/dc.c
@@ -1452,9 +1452,6 @@ static void _tegra_dc_controller_disable(struct tegra_dc *dc)
 {
 	disable_irq(dc->irq);
 
-	if (dc->overlay)
-		tegra_overlay_disable(dc->overlay);
-
 	if (dc->out_ops && dc->out_ops->disable)
 		dc->out_ops->disable(dc);
 
@@ -1480,6 +1477,9 @@ static void _tegra_dc_disable(struct tegra_dc *dc)
 
 void tegra_dc_disable(struct tegra_dc *dc)
 {
+	if (dc->overlay)
+		tegra_overlay_disable(dc->overlay);
+
 	mutex_lock(&dc->lock);
 
 	if (dc->enabled) {
@@ -1783,6 +1783,9 @@ static int tegra_dc_suspend(struct nvhost_device *ndev, pm_message_t state)
 	struct tegra_dc *dc = nvhost_get_drvdata(ndev);
 
 	dev_info(&ndev->dev, "suspend\n");
+
+	if (dc->overlay)
+		tegra_overlay_disable(dc->overlay);
 
 	mutex_lock(&dc->lock);
 
