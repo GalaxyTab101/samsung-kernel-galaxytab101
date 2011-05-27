a2287a12ac5fbfe1f7ef92c0825726e2cbe04b1a
Tom Cherry <tcherry@nvidia.com>
2011-05-26 19:43:45 -0700

    sound: soc: tegra: add dai for i2s_device0
    Change-Id: Ib0662de97f4f98461f1003747340bb9e16f96ebb
    Reviewed-on: http://git-master/r/34258
    Tested-by: Ravindra Lokhande <rlokhande@nvidia.com>
    Reviewed-by: Ravindra Lokhande <rlokhande@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-by: Thomas Cherry <tcherry@nvidia.com>
    Tested-by: Thomas Cherry <tcherry@nvidia.com>

--- sound/soc/tegra/tegra_soc_wm8903.c
+++ sound/soc/tegra/tegra_soc_wm8903.c
@@ -544,19 +544,23 @@ wm8903_init_fail:
 }
 
 static struct snd_soc_dai_link tegra_soc_dai[] = {
+#if defined(CONFIG_ARCH_TEGRA_2x_SOC)
 	TEGRA_CREATE_SOC_DAI_LINK("WM8903", "WM8903 HiFi",
 		&tegra_i2s_dai[0], &wm8903_dai, &tegra_hifi_ops),
 
-#if defined(CONFIG_ARCH_TEGRA_2x_SOC)
 	TEGRA_CREATE_SOC_DAI_LINK("Tegra-generic", "Tegra Generic Voice",
 		&tegra_i2s_dai[1], &tegra_generic_codec_dai[1],
 		&tegra_voice_ops),
 #else
+	TEGRA_CREATE_SOC_DAI_LINK("WM8903", "WM8903 HiFi",
+		&tegra_i2s_dai[1], &wm8903_dai, &tegra_hifi_ops),
+
 	TEGRA_CREATE_SOC_DAI_LINK("Tegra-generic-0", "Tegra BB Voice",
-		&tegra_i2s_dai[1], &tegra_generic_codec_dai[1],
+		&tegra_i2s_dai[2], &tegra_generic_codec_dai[1],
 		&tegra_voice_ops),
+
 	TEGRA_CREATE_SOC_DAI_LINK("Tegra-generic-1", "Tegra BT Voice",
-		&tegra_i2s_dai[2], &tegra_generic_codec_dai[2],
+		&tegra_i2s_dai[3], &tegra_generic_codec_dai[2],
 		&tegra_voice_ops),
 
 #endif
